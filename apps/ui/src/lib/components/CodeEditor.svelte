8e9882c5a67ac55a65a7eb3f26046447975d0030
OptimusePrime <karlo.vizec@skole.hr>
2026-01-13 22:50:02 +0100

    added initial code editor setup

---
+++ apps/ui/src/lib/components/CodeEditor.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import * as monaco from "monaco-editor";
+    import { onMount } from "svelte";
+
+    let { code = "", language = "python" } = $props();
+
+    let editorElement: HTMLDivElement;
+    let editor: monaco.editor.IStandaloneCodeEditor;
+    let model: monaco.editor.ITextModel;
+
+    function loadCode(code: string, language: string) {
+        model = monaco.editor.createModel(code, language);
+        editor.setModel(model);
+    }
+
+    onMount(() => {
+       editor = monaco.editor.create(editorElement);
+
+       loadCode(code, language)
+    });
+</script>
+
+<div class="flex-grow" bind:this={editorElement}></div>
+
+
---
+++ apps/ui/src/routes/problem/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import CodeEditor from "$lib/components/CodeEditor.svelte";
+
+    let code = `
+    import math
+
+    print(math.sqrt(10))
+    `
+</script>
+
+<CodeEditor code={code}></CodeEditor>
